177670acd7a1a43a2f5860ca344ddd8d3f9debd3
Christian Heckendorf <heckendorfc@gmail.com>
2014-01-13 22:35:00 -0500

    Updating stats and affects windows.

--- start.sh
+++ start.sh
@@ -10,15 +10,14 @@ rm gstats
 mkfifo gstats
 
 #tmux new-session -s an 'tail -f chat' \;\
-	#splitw -h -l 80 'tail -f affects' \;\
-	#splitw -v -p 30 'tail -f cstats' \;\
-	#splitw -v -l 4 'tail -f gstats' \;\
-	#selectp -t 0 \;\
 	#splitw -v -p 85 "tt++ -r $1" \;\
 	#selectp -t1 \;\
 	#attach
 
 tmux new-session -s an 'tail -f chat' \;\
+	splitw -h -l 80 'tail -f affects' \;\
+	splitw -v -l 15 'tail -f cstats' \;\
+	selectp -t 0 \;\
 	splitw -v -p 85 "tt++ -r $1" \;\
 	selectp -t1 \;\
 	attach
