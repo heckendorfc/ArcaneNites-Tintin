4708aa9d6c6dcdb2cf194695deeb1abd06a2fbf6
Christian Heckendorf <heckendorfc@gmail.com>
2014-01-20 18:44:54 -0500

    Using MSDP for combat, adding local and server maps to UI.

--- start.sh
+++ start.sh
@@ -9,6 +9,12 @@ mkfifo cstats
 rm gstats
 mkfifo gstats
 
+rm minimap
+mkfifo minimap
+
+rm ttmap
+mkfifo ttmap
+
 #tmux new-session -s an 'tail -f chat' \;\
 	#splitw -v -p 85 "tt++ -r $1" \;\
 	#selectp -t1 \;\
@@ -17,6 +23,9 @@ mkfifo gstats
 tmux new-session -s an 'tail -f chat' \;\
 	splitw -h -l 80 'tail -f affects' \;\
 	splitw -v -l 15 'tail -f cstats' \;\
+	splitw -h -l 15 'tail -f minimap' \;\
+	selectp -t 3 \;\
+	splitw -h -l 30 'tail -f ttmap' \;\
 	selectp -t 0 \;\
 	splitw -v -p 85 "tt++ -r $1" \;\
 	selectp -t1 \;\
