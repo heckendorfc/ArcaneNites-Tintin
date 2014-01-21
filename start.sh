b46272bdcf8f4170e7cc47724564a4df12708e26
Christian Heckendorf <heckendorfc@gmail.com>
2014-01-20 21:18:19 -0500

    Fixing start script windows.

--- start.sh
+++ start.sh
@@ -23,9 +23,8 @@ mkfifo ttmap
 tmux new-session -s an 'tail -f chat' \;\
 	splitw -h -l 80 'tail -f affects' \;\
 	splitw -v -l 15 'tail -f cstats' \;\
+	splitw -h -l 45 'tail -f ttmap' \;\
 	splitw -h -l 15 'tail -f minimap' \;\
-	selectp -t 3 \;\
-	splitw -h -l 30 'tail -f ttmap' \;\
 	selectp -t 0 \;\
 	splitw -v -p 85 "tt++ -r $1" \;\
 	selectp -t1 \;\
